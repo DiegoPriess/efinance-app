279001db4324b0963776d56cd4cb556700ed7e35
Gabriel Furquim da Silva <88550409+Gabriel7fs@users.noreply.github.com>
2023-04-13 19:39:33 -0300

    feat: tela-principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './page/home/home.component';
 import { PoModule } from '@po-ui/ng-components';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PoModule,
     HttpClientModule,
-    HomeComponent,
     RouterModule.forRoot([]),
   ],
   providers: [],
