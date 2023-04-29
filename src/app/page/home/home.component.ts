e57c35c6d3591e7e01db16a80f58313b51ec5ed2
Diego Priess <diegopriess.dev@gmail.com>
2023-04-29 03:17:18 -0300

    Adicionando componente form-login ao home

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -3,19 +3,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormLoginComponent } from "../../component/form-login/form-login.component";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  standalone: true,
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatFormFieldModule
-  ],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    standalone: true,
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        FormLoginComponent
+    ]
 })
 export class HomeComponent implements OnInit {
 
