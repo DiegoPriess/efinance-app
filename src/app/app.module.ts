a94fc8126aaecb1dedd12219d5f18168e2039728
Diego Priess <diegopriess.dev@gmail.com>
2023-04-29 02:28:13 -0300

    Alterando tela home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { PoModule } from '@po-ui/ng-components';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from './page/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { HomeComponent } from "./page/home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    PoModule,
-    HttpClientModule,
-    RouterModule.forRoot([]),
-    BrowserAnimationsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        PoModule,
+        HttpClientModule,
+        RouterModule.forRoot([]),
+        BrowserAnimationsModule,
+        HomeComponent
+    ]
 })
 export class AppModule {}
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule
+  ],
 })
 export class HomeComponent implements OnInit {
 
