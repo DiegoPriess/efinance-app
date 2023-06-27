4387b2807c849614aaa6cf1022d1653a743dbaf9
Diego Priess <diegopriess.dev@gmail.com>
2023-06-27 16:45:50 -0300

    Adicionando validações navbar

--- src/app/component/historic/historic.component.ts
+++ src/app/component/historic/historic.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./historic.component.scss'],
   standalone: true
 })
-export class HistoricComponent {
-
-}
+export class HistoricComponent {}
--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -14,6 +14,7 @@ import { navbarState } from './interfaces/NavbarState';
   ]
 })
 export class NavbarComponent{
+  userId : string | null = localStorage.getItem('userId');
   state: navbarState = navbarState.simulation;
 
   simulation: navbarState = navbarState.simulation;
